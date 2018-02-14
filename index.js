14220e712e4cc734e7b0edea2d721f174f33f2c2
Eduardo <eduardo_felipe_oliveira@yahoo.com.br>
2018-02-14 19:27:38 -0200

    Adição de conexão ao banco

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const port = process.env.port || 3000
+const url = 'mongodb://192.168.3.30:27017'
+const dbName = 'meu-dinheiro'
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
@@ -27,6 +32,56 @@ app.get('/calculadora', (req, res) => {
     res.render('calculadora', {resultado})
 })
 
-app.listen(port, () => console.log('Server running...'))
+const findAll = (db, collectionName) => {
+    const collection = db.collection(collectionName)
+    const cursor = collection.find({})
+    const documents = []
 
-// Fullstack Academy aula 02 - 2:01:13
+    return new Promise((resolve, reject) => {
+        cursor.forEach(
+            (doc) => documents.push(doc),
+            () => resolve(documents)
+        )
+    })
+}
+
+const insert = (db, collectionName, document) => {
+    const collection = db.collection(collectionName)
+    return new Promise((resolve, reject) => {
+        collection.insert(document, (err, doc) => {
+            if(err){
+                reject(err)
+            }else{
+                resolve(doc)
+            }
+        })
+    })
+}
+
+app.get('/operacoes', async (req, res) => {
+    const operacoes = await findAll(app.db, 'operacoes')
+    res.render('operacoes', {operacoes})
+})
+
+app.get('/nova-operacao', (req, res) => {
+    res.render('nova-operacao')
+})
+app.post('/nova-operacao', async (req, res) => {
+    const operacao = {
+        descricao: req.body.descricao,
+        valor: parseFloat(req.body.valor)
+    }
+    const newOperacao = await insert(app.db, 'operacoes', operacao)
+    res.redirect('/operacoes')
+})
+
+MongoClient.connect(url, (err, client) => {
+    if(err){
+        return
+    }else{
+        app.db = client.db(dbName)
+        app.listen(port, () => console.log('Server running...'))
+    }
+})
+
+// Aula 2 02:58:54
